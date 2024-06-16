3eb973feee16216efafb853571cbf7e7246be615
khushveer <khushveermalviya26@gmail.com>
2024-06-17 00:07:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Nav from './Components/Nav'
+
+function App() {
+ 
+  return (
+    <div className='bg-slate-600 h-screen  '>
+     <Nav />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='w-full h-10 flex justify-start ml-3'>
+          <img src="/logo.svg" className="h-10 w-10 text-white mt-7 ml-10" alt="Logo" />
+        <h1 className='text-2xl text-black font-bold mt-7 ml-4'>Elephanna</h1>
+      
+
+
+    </div>
+  )
+}
