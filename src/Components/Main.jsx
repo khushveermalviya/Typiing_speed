cfa5c0481d678ebb2186782188bc80ab51c3a2a4
khushveer <khushveermalviya26@gmail.com>
2024-06-24 00:45:41 +0530

    second day

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,18 +1,23 @@
-import { useRef,useState } from "react";
-const randomWords =()=> "apple banana cherry date elderberry fig grapefruit huckleberry iceplant jackfruit kiwi lemon mango nectarine orange papaya quince raspberry strawberry tangerine ume vanilla watermelon xigua yew zucchini alpha beta gamma delta epsilon zeta eta theta iota kappa lambda mu nu xi omicron pi rho sigma tau upsilon phi chi psi omega".split(" ").sort(()=>Math.random()-0.5);
+import { useRef, useState } from "react";
+import { faker } from '@faker-js/faker';
+
+const words = faker.lorem.words(100);
+
+function Generter({ words }) {
+  return <h1 className="text-white text-2xl m-7">{words}|</h1>;
+}
+
+function Countimer({ timeLeft }) {
+  return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>;
+}
 
 export default function Main() {
-   const [useInput,setUserInput]=useState('');
-   const cloud=useRef(randomWords());
-   console.log(useInput);
-    return (
+  console.log(words);
+  return (
+    <div className="w-screen">
     
-    <div className="flex">
-
-       <h1 className="text-white text-2xl m-7">{cloud.current.join('')}|</h1>
-       <input type='text' value={useInput} onChange={(e)=>setUserInput(e.target.value)} />
+      <Countimer timeLeft={30} />
+      <Generter words={words} />
     </div>
-    
-    
-  )
+  );
 }
